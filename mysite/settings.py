5c7242145663b0deb48f26c0bbafd1bea04093cc
SanderBloem <sbl03m@gmail.com>
2022-10-24 22:05:46 +0200

    removed secret from settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import configparser
 from pathlib import Path
+
+config = configparser.ConfigParser()
+config.read('secrets.ini')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8uyj8wlv&3w1-lw2#&5c409e&)81(+e+1%b5853%#0oj0owyn)'
+SECRET_KEY = config['settings']['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
