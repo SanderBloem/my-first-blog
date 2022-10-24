3c0508b1a05a1b4e74154856e8885714605377b3
SanderBloem <sbl03m@gmail.com>
2022-10-24 22:56:50 +0200

    secrets.ini gewijzigd in secret-key.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import configparser
 from pathlib import Path
-
-config = configparser.ConfigParser()
-config.read('secrets.ini')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['settings']['SECRET_KEY']
+with open('secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
